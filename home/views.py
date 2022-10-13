e62dc8f691bea01e00f10d07c9460e6dca9d655b
Hunter Bendel <70610577+HunterBendel@users.noreply.github.com>
2022-10-12 19:54:33 -0700

    Update views.py
    Created routes to get to the other pages on the website.

--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ home_view = Blueprint('home_view', __name__)
 @home_view.route('/')  # Route for the page
 def display_home_page():
 	return render_template('home_page.html', num=10)
+
+@home_view.route('/profile_page') # Route for the profile page
+def display_profile_page():
+	return render_template('profile_page.html', num=10)
+
+@home_view.route('/settings_page') # Route for the settings page
+def display_settings_page():
+	return render_template('settings_page.html', num=10)
